eba6fc7aed380aa432a691f81bfab74499c9e0ea
Alpha Bah <alphabah@MacBook-Pro-de-Alpha.local>
2024-02-23 16:17:23 +0300

    add facebook login

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,9 +1,20 @@
-import { Link } from "react-router-dom";
-import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { Link, resolvePath } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Divider,
+  Header,
+  Image,
+  Segment,
+} from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoginForm from "../users/LoginForm";
 import RegisterForm from "../users/RegisterForm";
+import FacebookLogin, {
+  FailResponse,
+  SuccessResponse,
+} from "@greatsumini/react-facebook-login";
 
 export default observer(function HomePage() {
   const { userStore, modalStore } = useStore();
@@ -42,6 +53,22 @@ export default observer(function HomePage() {
             >
               Register
             </Button>
+            <Divider horizontal inverted>
+              Or
+            </Divider>
+            <FacebookLogin
+              appId="7129066040503765"
+              onSuccess={(response: SuccessResponse) => {
+                userStore.facebookLogin(response.accessToken);
+                console.log("login success", response);
+              }}
+              onFail={(response: FailResponse) => {
+                console.log("Login Failed!", response);
+              }}
+              className={`ui button facebook huge inverted ${
+                userStore.fbLoading && "loading"
+              }`}
+            />
           </>
         )}
       </Container>
